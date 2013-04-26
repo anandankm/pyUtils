255394aa774c577cb04c712789db9ae734117594
andy <andy.compeer@gmail.com>
2013-04-26 11:46:06 -0400

    Write a file given an list of strings and a filename

--- file_utils.py
+++ file_utils.py
@@ -19,3 +19,8 @@ def get_file(filename, mode='r'):
         sys.stderr.write("ERROR: Cannot read input test file: %s.\nError msg: %s.\n" % (filename, e.strerror))
         raise
     return file_handle
+
+def write_output(array, filename):
+    file_h = get_file(filename, "w")
+    for item in array:
+        file_h.write(item + "\n")
