6215226c5b82fe5dfd5be2d67aba78c92ea8435c
andy <andy.compeer@gmail.com>
2013-04-30 20:10:43 -0400

    Reading and writing json strings to gzip files.

--- file_utils.py
+++ file_utils.py
@@ -3,8 +3,7 @@
 __author__="Anandan Rangasmay <andy.compeer@gmail.com>"
 __date__ ="$Apr 25, 2013"
 
-import sys
-import json
+import sys, json, gzip
 
 def sentence_itr(file_handle):
     words = []
@@ -12,6 +11,15 @@ def sentence_itr(file_handle):
         words = l.strip().split()
         if len(words) > 0:
             yield words
+
+""" Defaults to binary read """
+def get_gzip(filename, mode='rb'):
+    try:
+        file_handle = gzip.open(filename, mode)
+    except IOError as e:
+        sys.stderr.write("ERROR: Cannot read input test file: %s.\nError msg: %s.\n" % (filename, e.strerror))
+        raise
+    return file_handle
 
 def get_file(filename, mode='r'):
     try:
@@ -21,13 +29,18 @@ def get_file(filename, mode='r'):
         raise
     return file_handle
 
+""" Compress some big content (string obj) into a gzip file """
+def write_gzip(big_content, filename):
+    file_h = get_gzip(filename, "wb")
+    file_h.write(big_content)
+    file_h.close()
+
+""" Write an obj (better to be a list or a dict) represented as json string to a gzip file """
+def write_json_gzip(obj, filename):
+    write_gzip(json.dumps(obj), filename)
+
 def write_output(array, filename):
     file_h = get_file(filename, "w")
     for item in array:
         file_h.write(item + "\n")
-
-""" Write an obj (better to be a list or a dict) represented as json string to a file """
-def write_json(obj, filename):
-    file_h = get_file(filename, "w")
-    json_string = json.dumps(obj)
-    file_h.write(json_string)
+    file_h.close()
