24d2d36eba9829b0168bfd6a2829a60853bf16aa
anandan <andy.compeer@gmail.com>
2013-05-13 19:24:32 -0400

    Set and start the threads separately. Store threads in a dict

--- lib/readthreads.py
+++ lib/readthreads.py
@@ -9,13 +9,29 @@ import file_utils
 
 
 class readThread(object):
-    def __init__(self, f):
+    def __init__(self, f, nt=4):
         self.ifile = f
-        self.thdsperfile = 4
+        self.thdsperfile = nt
         self.totlines = file_utils.nlines(self.ifile)
-        self.thds = []
+        self.thds = {}
         self.sharedResource = {}
-        self.running = 0
+        self.isrun = 0
+        self.setThreads()
+
+    def setThreads(self):
+        threadname = "Thread-"
+        s = 1
+        n = self.totlines/self.thdsperfile
+        rem = self.totlines%self.thdsperfile
+        k = 1
+        while k <= self.thdsperfile:
+            thrdname = threadname + str(k)
+            if k == self.thdsperfile:
+                n += rem
+            thread = self.fileThread(thrdname, self.ifile, s, n, self.sharedResource)
+            self.thds[thread] = 1
+            s += n
+            k += 1
 
     def getSharedResource(self):
         return self.sharedResource
@@ -31,7 +47,8 @@ class readThread(object):
             self.counter = 0
 
         def getnext(self):
-            line = linecache.getline(self.fname, self.counter + self.s)
+            lineno = self.counter + self.s
+            line = linecache.getline(self.fname, lineno)
             if line:
                 words = line.strip().split()
                 self.shared[words[0]] = words
@@ -41,29 +58,23 @@ class readThread(object):
             while self.counter < self.nlines:
                 self.getnext()
 
+    def printThreads(self):
+        for thd in self.thds.keys():
+            print thd.name, thd.s, thd.nlines + thd.s, thd.nlines
 
     def startThreads(self):
-        threadname = "Thread-"
-        s = 1
-        n = self.totlines/self.thdsperfile
-        rem = self.totlines%self.thdsperfile
-        k = 1
-        while k <= self.thdsperfile:
-            thrdname = threadname + str(k)
-            if k == self.thdsperfile:
-                n += rem
-            thread = self.fileThread(thrdname, self.ifile, s, n, self.sharedResource)
-            self.thds.append(thread)
-            thread.start()
-            self.running += 1
-            s += n
-            k += 1
+        for thd in self.thds.keys():
+            thd.start()
+            self.isrun += 1
+            print "Started", thd.name
 
     def joinThreads(self):
-        while self.running > 0:
-            for thd in self.thds:
+        while self.isrun > 0:
+            for thd in self.thds.keys():
                 if not thd.isAlive():
-                    self.running -= 1
-                    print thd.name, "completed."
+                    if self.thds[thd] == 1:
+                        self.isrun -= 1
+                        self.thds[thd] = 0
                 else:
                     thd.join(0.25)
+        print "All threads completed."
